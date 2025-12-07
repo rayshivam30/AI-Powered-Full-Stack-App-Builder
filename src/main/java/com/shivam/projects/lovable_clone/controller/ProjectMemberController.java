e5e7ffde2cf7c307bdf2b7c21690503dbb58b42a
Shivam Ray <shivamray4292@gmail.com>
2025-12-07 23:41:28 +0530

    Part - 3, ProjectMemberServiceImpl added

--- src/main/java/com/shivam/projects/lovable_clone/controller/ProjectMemberController.java
+++ src/main/java/com/shivam/projects/lovable_clone/controller/ProjectMemberController.java
@@ -3,7 +3,6 @@ package com.shivam.projects.lovable_clone.controller;
 import com.shivam.projects.lovable_clone.dto.member.InviteMemberRequest;
 import com.shivam.projects.lovable_clone.dto.member.MemberResponse;
 import com.shivam.projects.lovable_clone.dto.member.UpdateMemberRoleRequest;
-import com.shivam.projects.lovable_clone.entity.ProjectMember;
 import com.shivam.projects.lovable_clone.service.ProjectMemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -47,21 +46,13 @@ public class ProjectMemberController {
     }
 
     @DeleteMapping("/{memberId}")
-    public ResponseEntity<MemberResponse> deleteMember(
+    public ResponseEntity<Void> removeMember(
             @PathVariable Long projectId,
             @PathVariable Long memberId
     ) {
         Long userId = 1L;
-        return ResponseEntity.ok(projectMemberService.deleteProjectMember(projectId, memberId, userId));
+        projectMemberService.removeProjectMember(projectId, memberId, userId);
+        return ResponseEntity.noContent().build();
     }
-
-
-
-
-
-
-
-
-
 
 }
---
+++ src/main/java/com/shivam/projects/lovable_clone/repository/ProjectMemberRepository.java
@@ -0,0 +1,14 @@
+package com.shivam.projects.lovable_clone.repository;
+
+import com.shivam.projects.lovable_clone.entity.ProjectMember;
+import com.shivam.projects.lovable_clone.entity.ProjectMemberId;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProjectMemberRepository extends JpaRepository<ProjectMember, ProjectMemberId> {
+
+    List<ProjectMember> findByIdProjectId(Long projectId);
+}
