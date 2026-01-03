84d5be9ff8cdca50d51d06f023b199089db3937c
Shivam Ray <shivamray4292@gmail.com>
2026-01-03 22:56:25 +0530

    Part - 6, Security methods and Authorization added

--- src/main/java/com/shivam/projects/lovable_clone/enums/ProjectRole.java
+++ src/main/java/com/shivam/projects/lovable_clone/enums/ProjectRole.java
@@ -1,5 +1,23 @@
 package com.shivam.projects.lovable_clone.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Set;
+
+import static com.shivam.projects.lovable_clone.enums.ProjectPermission.*;
+
+@RequiredArgsConstructor
+@Getter
 public enum ProjectRole {
-    EDITOR, VIEWER, OWNER
+
+    EDITOR(VIEW, EDIT, DELETE, VIEW_MEMBERS),
+    VIEWER(Set.of(VIEW, VIEW_MEMBERS)),
+    OWNER(Set.of(VIEW, EDIT, DELETE, MANAGE_MEMBERS, VIEW_MEMBERS));
+
+    ProjectRole(ProjectPermission... permissions) {
+        this.permissions = Set.of(permissions);
+    }
+
+    private final Set<ProjectPermission> permissions;
 }
