2791b4d4195a219639c3554ebdb12d3b10fc5d33
Shivam Ray <shivamray4292@gmail.com>
2026-01-10 23:50:23 +0530

    Part - 8, Subscription webhook handlers added - I

--- src/main/java/com/shivam/projects/lovable_clone/repository/PlanRepository.java
+++ src/main/java/com/shivam/projects/lovable_clone/repository/PlanRepository.java
@@ -1,7 +1,11 @@
 package com.shivam.projects.lovable_clone.repository;
 
+import aj.org.objectweb.asm.commons.Remapper;
 import com.shivam.projects.lovable_clone.entity.Plan;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PlanRepository extends JpaRepository<Plan, Long> {
+    Optional<Plan> findByStripePriceId(String id);
 }
