e4b3140d660a9cc477324c8a9c8156e73da826ad
Shivam Ray <shivamray4292@gmail.com>
2026-01-19 01:01:37 +0530

    Part - 11, AI code generation

--- src/main/java/com/shivam/projects/lovable_clone/controller/FileController.java
+++ src/main/java/com/shivam/projects/lovable_clone/controller/FileController.java
@@ -2,7 +2,7 @@ package com.shivam.projects.lovable_clone.controller;
 
 import com.shivam.projects.lovable_clone.dto.project.FileContentResponse;
 import com.shivam.projects.lovable_clone.dto.project.FileNode;
-import com.shivam.projects.lovable_clone.service.FileService;
+import com.shivam.projects.lovable_clone.service.ProjectFileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,12 +17,12 @@ import java.util.List;
 @RequestMapping("/api/projects/{projectId}/files")
 public class FileController {
 
-    private final FileService fileService;
+    private final ProjectFileService projectFileService;
 
     @GetMapping
     public ResponseEntity<List<FileNode>> getFileTree(@PathVariable Long projectId) {
         Long userId = 1L;
-        return ResponseEntity.ok(fileService.getFileTree(projectId, userId));
+        return ResponseEntity.ok(projectFileService.getFileTree(projectId, userId));
     }
 
     @GetMapping("/{*path}") // /src/hooks/get-user-hook.jsx
@@ -31,7 +31,7 @@ public class FileController {
             @PathVariable String path
     ) {
         Long userId = 1L;
-        return ResponseEntity.ok(fileService.getFileContent(projectId, path, userId));
+        return ResponseEntity.ok(projectFileService.getFileContent(projectId, path, userId));
     }
 
 }
